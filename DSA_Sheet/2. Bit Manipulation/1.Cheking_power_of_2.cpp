7307181e226ab108e5b74a33503351a884a82424
ActionJags95 <actionjags@gmail.com>
2024-01-09 00:00:02 +0530

    Made a small change

--- DSA_Sheet/2. Bit Manipulation/1.Cheking_power_of_2.cpp
+++ DSA_Sheet/2. Bit Manipulation/1.Cheking_power_of_2.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 bool Power_of_2(int n){
-    while(n>0){
-        if(n%2==0){return true;}
-        else if(n%2==1){return false;}
+    while(n>1){
+        if(n%2==1){return false;}
         else{n/=2;}
     }
+    return true;
 }
 int main(){
     int n;
